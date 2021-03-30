c84829ca328c260139ede0ecd53bbf8010110d4a
tmosco <tmosco_tayo@yahoo.com>
2021-03-30 18:40:02 +0100

    create an updateObject function for the reducers

--- z_others.js
+++ z_others.js
@@ -18,3 +18,68 @@ for (let key in this.state.orderForm) {
     id: key,
     config: this.state.orderForm[key],
   });
+
+
+
+
+  import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../utility";
+
+const initialState = {
+  ingredients: null,
+  totalPrice: 4,
+  error: false,
+};
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_INGREDIENT:
+      const updatedIngredient = {
+        [action.ingredientName]: state.ingredients[action.ingredientName] + 1,
+      };
+      const updatedIngredients = updateObject(
+        state.ingredients,
+        updatedIngredient
+      );
+      const updatedState = {
+        ingredients: updatedIngredients,
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+      };
+      return updateObject(state, updatedState);
+  
+  
+      case actionTypes.REMOVE_INGREDIENT:
+      const updatedIng = {
+        [action.ingredientName]: state.ingredients[action.ingredientName] - 1,
+      };
+      const updatedIngs = updateObject(state.ingredients, updatedIng);
+      const updatedStates = {
+        ingredients: updatedIngs,
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+      };
+      return updateObject(state, updatedStates);
+  
+  
+      case actionTypes.SET_INGREDIENTS:
+      return updatedState(state, {
+        ingredients: action.ingredients,
+        totalPrice: 4,
+        error: false,
+      });
+
+    case actionTypes.FETCH_INGREDIENTS_FAILED:
+      return updateObject(state, { error: true });
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
