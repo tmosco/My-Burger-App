aa5ec7d0344b5c5d1e3e8bc370d58f6cb4c671c1
tmosco <tmosco_tayo@yahoo.com>
2021-04-01 13:01:45 +0100

    changed just the app.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,45 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import Layouts from "./components/Layout/Layouts";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
-import { Route, Switch, withRouter,Redirect } from "react-router-dom";
+import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 import Orders from "./containers/Orders/Orders";
 import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout";
 import * as actions from "./Store/actions/index";
 
-class App extends Component {
-  componentDidMount() {
-    this.props.onTryAutoSignup();
-  }
+const App = (props) => {
+  useEffect(() => {
+    props.onTryAutoSignup();
+  }, []);
 
-  render() {
-    let routes = (
+  let routes = (
+    <Switch>
+      <Route path="/auth" exact component={Auth} />
+      <Route path="/" exact component={BurgerBuilder} />
+      <Redirect to="/" />
+    </Switch>
+  );
+  if (props.isAuthenticated) {
+    routes = (
       <Switch>
+        <Route path="/logout" component={Logout} />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/orders" exact component={Orders} />
         <Route path="/auth" exact component={Auth} />
         <Route path="/" exact component={BurgerBuilder} />
-        <Redirect to="/"/>
+        <Redirect to="/" />
       </Switch>
     );
-    if (this.props.isAuthenticated) {
-      routes = (
-        <Switch>
-          <Route path="/logout" component={Logout} />
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" exact component={Orders} />
-          <Route path="/auth" exact component={Auth} />
-          <Route path="/" exact component={BurgerBuilder} />
-          <Redirect to="/"/>
-        </Switch>
-      );
-    }
-
-    return (
-      <>
-        <Layouts>{routes}</Layouts>
-      </>
-    );
   }
-}
+
+  return (
+    <>
+      <Layouts>{routes}</Layouts>
+    </>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
--- utils.js
+++
@@ -1,7 +0,0 @@
-// const expirationDate = new Date(new Date().getTime() + 36  * 1000)
-const expirationDate =new Date( new Date().getTime() + 36  * 1000)
-
-
-console.log(expirationDate);
-
-
